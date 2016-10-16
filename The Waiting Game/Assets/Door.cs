bd4ee9a84cf8ba6f3443c0f68c106f81f90cc9be
Nelson <nelson.alemar@upr.edu>
2016-10-16 10:02:36 -0400

    Flip bottle done. Trying to implement open door

---
+++ The Waiting Game/Assets/Door.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    int delay = 5000;
+    static bool appLoaded = false;
+    int i = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (appLoaded == false)
+        {
+            GetComponent<Animation>().Play("open");
+            if (!GetComponent<Animation>().IsPlaying("open"))
+            {
+                Application.LoadLevel("BottleFlip");
+                i++;
+            }
+
+            appLoaded = true;
+        }
+
+        else
+        {
+            GetComponent<Animation>().Play("close");
+            }
+        }
+	}
+
