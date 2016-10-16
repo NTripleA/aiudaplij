5ffa6be0dd9bf383b0d968f7b049b88f3b6730a4
Nelson <nelson.alemar@upr.edu>
2016-10-16 12:59:02 -0400

    AHHHHH

--- The Waiting Game/Assets/Door.cs
+++ The Waiting Game/Assets/Door.cs
@@ -1,36 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour {
 
     int delay = 5000;
-    static bool appLoaded = false;
-    int i = 0;
+    bool appLoaded = false;
+    bool done = false, playing = false;
+    static int i = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (appLoaded == false)
+        if (!appLoaded)
         {
-            GetComponent<Animation>().Play("open");
-            if (!GetComponent<Animation>().IsPlaying("open"))
-            {
-                Application.LoadLevel("BottleFlip");
-                i++;
+            
+
+            if (done)
+            {   i++;
+                appLoaded = true;
+                if (i == 1)
+                    SceneManager.LoadScene("BottleFlip");
+                else if (i == 2)
+                    SceneManager.LoadScene("MiniTank/Missile-scene");
+                else
+                {
+                    i = 0;
+                }
+                
             }
 
-            appLoaded = true;
+            else
+            {
+                if(!GetComponentInChildren<Animation>().IsPlaying("open") && !playing)
+                    GetComponentInChildren<Animation>().Play("open");
+
+                playing = true;
+            }
+
+            if (!GetComponentInChildren<Animation>().IsPlaying("open"))
+            {
+                done = true;
+            }
+
+
         }
 
         else
         {
-            GetComponent<Animation>().Play("close");
+            GetComponentInChildren<Animation>().Play("close");
+            appLoaded = false;
             }
         }
-	}
+
+        IEnumerator MyMethod()
+        {
+            Debug.Log("Before Waiting 2 seconds");
+            yield return new WaitForSeconds(2);
+            Debug.Log("After Waiting 2 Seconds");
+        }
+}
 
