c0326a3923966ecbdc1dabace6b2d378cce97a4c
CarlosRivera87 <cturboso@hotmail.com>
2016-10-16 09:00:07 -0400

    Mini Tanks game scene upload HACKPR2016

---
+++ The Waiting Game/MiniTank/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	public int bullet_speed;
+	Rigidbody bullet;
+	// Use this for initialization
+	void Start () {
+		bullet = GetComponent<Rigidbody>();
+		bullet.velocity = new Vector3 (0, bullet_speed, 0);
+		bullet.transform.localScale = new Vector3 (400, 400, 400);
+		bullet.transform.Rotate(new Vector3(-90,0,0));
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if (bullet.transform.position.y > 200) {
+			Destroy (this.gameObject);
+		}
+	}
+
+}
